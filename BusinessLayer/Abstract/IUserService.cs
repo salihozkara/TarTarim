7d9bea3c2dff52384dc76c6bf66e76821e662123
Salih <salihozkara00@gmail.com>
2021-12-03 19:11:00 +0300

    auth and login to created

---
+++ BusinessLayer/Abstract/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using CoreLayer.Entities.Concrete;
+using CoreLayer.Utilities.Results;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IUserService
+    {
+        IDataResult<List<OperationClaim>> GetClaims(User user);
+        IResult Add(User user);
+        IDataResult<User> GetByMail(string email);
+        IResult Update(User user);
+        IDataResult<User> GetById(int userId);
+    }
+}
---
+++ CoreLayer/Entities/DTOs/UserForLoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreLayer.Entities.DTOs
+{
+    public class UserForLoginDto : IDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
