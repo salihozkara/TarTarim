1c1a2c90c9069cf29caba7c886b4af2f2b83b2b9
Muhammed Ali OZKAYA <m_aliozkaya@hotmail.com>
2021-12-04 05:58:08 +0300

    Category added icon

--- EntityLayer/Concrete/QuestionCategory.cs
+++ EntityLayer/Concrete/QuestionCategory.cs
@@ -11,6 +11,8 @@ namespace EntityLayer.Concrete
         public int Id { get; set; }
         [StringLength(50)]
         public string Name { get; set; }
+        [StringLength(50)]
+        public string Icon { get; set; }
         public int Order { get; set; }
     }
 }
